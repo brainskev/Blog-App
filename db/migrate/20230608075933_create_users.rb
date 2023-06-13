e6b2b9116aa9719661594a666fbccca5c8eb04b4
Kelvin <ngotho.kelvin@yahoo.com>
2023-06-13 12:34:44 +0300

    Reconfigure migration files

--- db/migrate/20230608075933_create_users.rb
+++ db/migrate/20230608075933_create_users.rb
@@ -3,8 +3,10 @@ class CreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       t.string :name
       t.string :photo
-      t.integer :bio
+      t.text :bio
       t.integer :post_counter, default: 0
+
+      t.timestamps
     end
   end
 end
