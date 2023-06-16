71ea961d6f4ecf4a1dfc436bf15b0a45d5f3d2d3
Kelvin <ngotho.kelvin@yahoo.com>
2023-06-16 17:32:09 +0300

    Add validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,12 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
+  def update_comments_conuter
+    post.increment!(:comments_cuonter)
+  after_create :update_comments_counter
+
+  private
+
   def update_comments_counter
     post.increment!(:comments_counter)
   end
