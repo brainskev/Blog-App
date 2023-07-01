140d585569cd99792ff7d7803cf3dd68f2863f32
Kelvin <ngotho.kelvin@yahoo.com>
2023-07-01 21:12:56 +0300

    Add post decrement method

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_many :comments
-  has_many :likes
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
@@ -11,11 +11,16 @@ class Post < ApplicationRecord
     comments.last(5)
   end
 
-  after_create :update_post_counter
+  after_destroy :decrement_post_counter
 
   private
 
   def update_post_counter
     author.increment!(:post_counter)
   end
+end
+
+def decrement_post_counter
+  author.decrement!(:post_counter)
+end
 end
